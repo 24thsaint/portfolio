2fb5e9c899860d6a01f58831e4db16045b3f2c97
24thsaint <rave.arevalo@kingsland.io>
2020-05-18 04:14:04 +0800

    build(app): specify node version

--- build-server/index.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import path from 'path';
-const app = express();
-app.use('/', express.static(path.join('build')));
-const PORT = process.env.PORT || '3030';
-app.listen(PORT, () => {
-    console.log(`Listening on PORT ${PORT}`);
-});

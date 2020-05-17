1ad7ce409a0e5fc8101c0a9b16eefb9ef4050078
24thsaint <rave.arevalo@kingsland.io>
2020-05-18 04:07:24 +0800

    feat(app): add express server

---
+++ build-server/index.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import path from 'path';
+const app = express();
+app.use('/', express.static(path.join('build')));
+const PORT = process.env.PORT || '3030';
+app.listen(PORT, () => {
+    console.log(`Listening on PORT ${PORT}`);
+});
